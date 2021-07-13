d9cafe773b5224053ac7d0bd0a9545c16dbf30a3
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 10:15:57 -0600

    Update app.py
    Commit

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import *
-import pandas as pd
+from flask import Flask
+import folium
+
 app = Flask(__name__)
 
-@app.route("/tables")
-def show_tables():
-    data = pd.read_excel('dummy_data.xlsx')
-    data.set_index(['Name'], inplace=True)
-    data.index.name=None
-    females = data.loc[data.Gender=='f']
-    males = data.loc[data.Gender=='m']
-    return render_template('view.html',tables=[females.to_html(classes='female'), males.to_html(classes='male')],
-    titles = ['na', 'Female surfers', 'Male surfers'])
+
+@app.route("/")
+def base():
+    # this is base map
+    map = folium.Map(
+        location=[-33.48621795345005, -70.66557950912359]
+    )
+    return map._repr_html_()
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
