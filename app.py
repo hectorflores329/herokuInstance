f128ef1b1d76e3211cdc8d6fa6f27e8b68b5132b
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 15:04:42 -0600

    Commit

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    map = folium.Map(location=[40.712776, -74.005974],zoom_start=10)
-    lat_long_list2 = [[[40.784217, -73.846007],[40.762797, -73.86511],[40.721856, -73.852954]],[40.647186, -73.980259]]
+    
+    map_Oslo = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=5)
+    HeatMapWithTime(df, 
+                            radius=10,
+                    gradient={0.1: 'blue', 0.2: 'blue', 0.4: 'lime', 0.6: 'orange', 0.8: 'red', 0.99: 'purple'},
+                    min_opacity=0.5, max_opacity=0.8, use_local_extrema=False).add_to(map_Oslo)
+    
 
-    # _map = HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
-
-    # return df.to_html(header="true", table_id="table")
-    folium.LayerControl().add_to(map)
-
-    return map._repr_html_()
+    return map_Oslo._repr_html_()
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
 if __name__ == '__main__':
