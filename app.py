f860d7e20fb984ca2cd1a88773a9848e62c0e612
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 14:45:14 -0600

    Commit

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ df = pd.DataFrame({'col1': ['abc', 'def', 'tre'],
 map = folium.Map(location=[40.712776, -74.005974],zoom_start=10)
 lat_long_list2 = [[[40.784217, -73.846007],[40.762797, -73.86511],[40.721856, -73.852954]],[40.647186, -73.980259]]
 
+HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
 
 app = Flask(__name__)
 
