098f495478932885191cc634cef65c5042798393
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 11:31:48 -0600

    Commit

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
 from flask import Flask
 import folium
+import pandas as pd
+
+# Importing Data
+df = pd.read_csv('mta_1712.csv', error_bad_lines=False)
+
+# Getting data for 1 December 2017
+df=df[df['RecordedAtTime'].str.split(' ').apply(lambda x:x[0]=='2017-12-01')]
+df.shape
+
+df=df[['RecordedAtTime','VehicleRef','VehicleLocation.Latitude','VehicleLocation.Longitude']]
+df.duplicated().value_counts()
+
+df=df.drop_duplicates()
+df.isnull().sum()
+
+df2=pd.DataFrame(df.groupby(['hour','VehicleRef'])['RecordedAtTime'].max())
+df2.reset_index(inplace=True)
+
+df3=pd.merge(df2,df,left_on=['hour','VehicleRef','RecordedAtTime'],right_on=['hour','VehicleRef','RecordedAtTime'])
+
+# Converting column to datetime
+df['RecordedAtTime']=pd.to_datetime(df['RecordedAtTime'],format='%Y-%m-%d %H:%M:%S')
+# Creating hour column
+df['hour']=df['RecordedAtTime'].apply(lambda x: x.hour+1)
+
+lat_long_list = []
+for i in range(1,25):
+    temp=[]
+    for index, instance in df3[df3['hour'] == i].iterrows():
+        temp.append([instance['VehicleLocation.Latitude'],instance['VehicleLocation.Longitude']])
+    lat_long_list.append(temp)
 
 app = Flask(__name__)
 
