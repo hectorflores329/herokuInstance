a165c46733a8e5b9f0bc8de9c76a4cf8973ca671
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 00:34:51 -0600

    Commit

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def index():
     return "Hola, este es un ejemplo."
 
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/nuevapagina")
+def index():
+    return "Hola, este es un ejemplo."
