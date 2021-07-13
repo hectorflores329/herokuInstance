eec4734006f2ce2e6efde619e2b575ecbb330f65
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 10:24:02 -0600

    Commit

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ def base():
     # this is base map
     map = folium.Map(
         location=[-33.48621795345005, -70.66557950912359],
-        width = 550,
-        height = 350,
-        zoom_start = 11,
+        width = 750,
+        height = 550,
+        zoom_start = 5,
         min_zoom = 8,
         max_zoom = 14
     )
+
+    folium.TileLayer('Stamen Terrain').add_to(map)
+    folium.TileLayer('Stamen Toner').add_to(map)
+    folium.TileLayer('Stamen Water Color').add_to(map)
+    folium.TileLayer('cartodbpositron').add_to(map)
+    folium.TileLayer('cartodbdark_matter').add_to(map)
+    folium.LayerControl().add_to(map)
+
     return map._repr_html_()
 
 
