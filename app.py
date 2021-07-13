628f18c2b83b211c0078020d1e9164f8daf2d6a7
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 15:44:14 -0600

    Commit

--- app.py
+++ app.py
@@ -274,11 +274,16 @@ def hello_world():
 
     '''HeatMapWithTime(lat_long_list2, radius=10, gradient={0.1: 'blue', 0.2: 'blue', 0.4: 'lime', 0.6: 'orange', 0.8: 'red', 0.99: 'purple'},
                     min_opacity=0.5, max_opacity=0.8, use_local_extrema=False, auto_play=True,position='bottomright').add_to(map_Oslo)'''
-    # HeatMapWithTime(lat_long_list2, radius=5, auto_play=True, position='bottomright').add_to(_map)
+
+    HeatMapWithTime(lat_long_list2, radius=5, auto_play=True, position='bottomright').add_to(_map)
     
 
     return _map._repr_html_()
     # return HeatMapWithTime(lat_long_list2,radius=5,auto_play=True,position='bottomright').add_to(map)
+
+@app.route('/tabla')
+def tabla():
+    return df.to_html(header="true", table_id="table")
 
 if __name__ == '__main__':
     app.run()
