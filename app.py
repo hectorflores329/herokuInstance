d6371fd72a0090680fe54e6e59b80d8ffff0b2c0
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 15:16:56 -0600

    Commit

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from folium.plugins import HeatMapWithTime
 df = pd.DataFrame({'col1': [10,20, 30],
                    'col2': [120, 150, 200]})
 
+lat_long_list2 = [[[40.784217, -73.846007],[40.762797, -73.86511],[40.721856, -73.852954]],[40.647186, -73.980259]]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +15,7 @@ def hello_world():
 
     
     map_Oslo = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=5)
-    HeatMapWithTime(df, radius=10).add_to(map_Oslo)
+    HeatMapWithTime(lat_long_list2, radius=10).add_to(map_Oslo)
     
 
     return map_Oslo._repr_html_()
