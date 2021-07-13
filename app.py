e0d22c8a245b6d900d93f160dffa6c4397d9badb
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 10:04:31 -0600

    Commit

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask
-import folium
+from flask import Flask, request, render_template, session, redirect
+import numpy as np
+import pandas as pd
+
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def base():
-    # this is base map
-    map = folium.Map(
-        location=[-33.48621795345005, -70.66557950912359]
-    )
-    return map._repr_html_()
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
 
 
-if __name__ == "__main__":
+@app.route('/', methods=("POST", "GET"))
+def html_table():
+
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+
+
+if __name__ == '__main__':
     app.run()
