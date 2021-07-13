baadbb9ef435ed05365640197205b72a9599f52c
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 00:42:29 -0600

    Commit

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from flask import Flask, request, render_template, session, redirect
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
+
 
 @app.route("/")
 def index():
@@ -10,6 +17,10 @@ def index():
 @app.route("/nuevapagina")
 def another():
     return "Hola, este es un ejemplo en otra página."
+
+@app.route("/dataframe")
+def table():
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 
 if __name__ == "__main__":
