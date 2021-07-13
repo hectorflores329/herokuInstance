d83e068b5b32cd1532bb726001263386d8af2ab8
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 15:30:33 -0600

    Commit

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ def hello_world():
         )
 
     HeatMapWithTime(lat_long_list2, radius=10, gradient={0.1: 'blue', 0.2: 'blue', 0.4: 'lime', 0.6: 'orange', 0.8: 'red', 0.99: 'purple'},
-                    min_opacity=0.5, max_opacity=0.8, use_local_extrema=False).add_to(map_Oslo)
+                    min_opacity=0.5, max_opacity=0.8, use_local_extrema=False, auto_play=True,position='bottomright').add_to(map_Oslo)
     
 
     return map_Oslo._repr_html_()
