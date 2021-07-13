7b031e5d72f51a1a52355f10ea4140b62ab7af36
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 14:22:50 -0600

    Commit

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
 from flask import Flask
-import folium
 import pandas as pd
-from folium import plugins
-from folium.plugins import HeatMap
+
+df = pd.DataFrame({'col1': ['abc', 'def', 'tre'],
+                   'col2': ['foo', 'bar', 'stuff']})
+
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return df.to_html(header="true", table_id="table")
 
-@app.route("/")
-def base():
-    df = pd.read_csv("mta_1712.csv")
-
-    # this is base map
-    map = folium.Map(
-        location=[-33.48621795345005, -70.66557950912359],
-        width = 750,
-        height = 550,
-        zoom_start = 5,
-        min_zoom = 8,
-        max_zoom = 14
-    )
-
-    folium.TileLayer('Stamen Terrain').add_to(map)
-    folium.TileLayer('Stamen Toner').add_to(map)
-    folium.TileLayer('Stamen Water Color').add_to(map)
-    folium.TileLayer('cartodbpositron').add_to(map)
-    folium.TileLayer('cartodbdark_matter').add_to(map)
-    folium.LayerControl().add_to(map)
-
-    return map._repr_html_()
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
