876b97c93b38a2f9d1a0720314ce64e14ce5ed8e
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 01:19:24 -0600

    Commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def base():
     # this is base map
     map = folium.Map(
-        location=[45.52336, -122.6750]
+        location=[-33.48621795345005, -70.66557950912359]
     )
     return map._repr_html_()
 
