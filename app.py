117ce440753653a776f20dd092ca95d5a991ac1a
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 15:11:26 -0600

    Commit

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ def hello_world():
 
     
     map_Oslo = folium.Map(location=[-33.48621795345005, -70.66557950912359], zoom_start=5)
-    HeatMapWithTime(df, 
-                            radius=10,
-                    gradient={0.1: 'blue', 0.2: 'blue', 0.4: 'lime', 0.6: 'orange', 0.8: 'red', 0.99: 'purple'},
-                    min_opacity=0.5, max_opacity=0.8, use_local_extrema=False).add_to(map_Oslo)
+   
     
 
     return map_Oslo._repr_html_()
