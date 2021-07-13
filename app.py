2b2a17cbccd8fc4b56958ef6fd6e28ba1a749cf0
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-13 01:44:39 -0600

    Commit

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 from flask import Flask
 import folium
-from django.shortcuts import render
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def show_map(request):
-    m = folium.Map(location=[55.17, 51.00], tiles='OpenStreetMap', name="Light Map",
-                   zoom_start=6, attr="My Data attribution")
-    folium.features.GeoJson(data= "all_id.geojson"
-                            , name="States", popup=folium.features.GeoJsonPopup(fields=["rname", "id"],
-                                                                                aliases=["region_name",
-                                                                                        "region_id"])).add_to(m)
-    m = m._repr_html_()
-    context = {'my_map': m}
-
-    return render(request, 'map/map_render.html', context) 
+def base():
+    # this is base map
+    map = folium.Map(
+        location=[-33.48621795345005, -70.66557950912359]
+    )
+    return map._repr_html_()
 
 
 if __name__ == "__main__":
