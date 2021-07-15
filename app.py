88355d3ca97880355684800fb8b11374961263df
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-07-15 01:27:18 -0600

    Commit

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def hello_world():
     folium.LayerControl().add_to(m)
 
     url = (
-        "https://raw.githubusercontent.com/SECOORA/static_assets/master/maps/img/rose.png"
+        "dataintelligence.png"
     )
 
     m = folium.Map([-13, -38.15], zoom_start=10)
